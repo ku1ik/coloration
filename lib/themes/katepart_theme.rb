7390e53e3505df25fc91c3a2ac7c38371cca7314
Marcin Kulik <marcin.kulik@gmail.com>
2009-12-16 01:04:24 +0100

    Implemented tm2kate, basic version

--- lib/themes/katepart_theme.rb
+++ lib/themes/katepart_theme.rb
@@ -9,61 +9,70 @@ module Coloration
     end
     
     def format_style(style)
-      # normal,selected,bold,italic,underline,strike,bg,bg_selected,---
+#       p style
+      # normal,selected,bold,italic,strike,underline,bg,bg_selected,---
       s = []
-      s << style[:foreground].to_s.downcase.gsub('#', '')
+      s << style.foreground.html.gsub('#', '')
       s << nil
-      s << (style[:bold] ? "1" : nil)
-      s << (style[:italic] ? "1" : nil)
-      s << (style[:underline] ? "1" : nil)
-      s << (style[:strike] ? "1" : nil)
-      s << style[:background].to_s.downcase.gsub('#', '')
+      s << (style.bold ? "1" : nil)
+      s << (style.italic ? "1" : nil)
+      s << (style.strike ? "1" : nil)
+      s << (style.underline ? "1" : nil)
+      s << (style.background ? style.background.html.gsub('#', '') : nil)
       s << nil
       s << "---"
-      s.join(",")
+      a = s.join(",")
+#       p a
+      a
+    end
+    
+    def format_item(name, style)
+      raise RuntimeError.new("Style for #{name} is missing!") if style.nil?
+      "#{name}=#{format_style(style)}"
     end
     
     def format_comment(text)
-      nil
-    end
-    
-    def format_comment(text)
-      "\# #{text}"
+      "# #{text}"
     end
     
     def build
-#       p @properties
-      add_comment "Put following into katesyntaxhighlightingrc"
+      add_comment "Put following into katesyntaxhighlightingrc\n"
+      
       add_line "[Default Item Styles - Schema #{name}]"
-      tokens = {
-        "Alert"          => :none, #
-        "Base-N Integer" => :number,
-        "Character"      => :operator,
-        "Comment"        => :comment,
-        "Data Type"      => :constant,
-        "Decimal/Value"  => :number,
-        "Error"          => :none, #
-        "Floating Point" => :number,
-        "Function"       => :function,
-        "Keyword"        => :keyword,
-        "Normal"         => :normal,
-        "Others"         => :none, #
-        "Region Marker"  => :none, #
-        "String"         => :string
+      
+      default_style = Style.new
+      default_style.foreground = @ui[:foreground]
+      
+      items = {
+        "Alert"          => @items["invalid"], #
+        "Base-N Integer" => @items["constant.numeric"],
+        "Character"      => @items["keyword.operator"],
+        "Comment"        => @items[:comment],
+        "Data Type"      => @items["entity.name.type"],
+        "Decimal/Value"  => @items["constant.numeric"],
+        "Error"          => @items["invalid"], #
+        "Floating Point" => @items["constant.numeric"],
+        "Function"       => @items["entity.name.function"],
+        "Keyword"        => @items["keyword"],
+        "Normal"         => default_style,
+        "Others"         => nil, #
+        "Region Marker"  => nil, #
+        "String"         => @items["string"]
       }
-      tokens.keys.each do |key|
-        add_line(format_token(key, self.send(tokens[key])))
+
+      items.keys.each do |key|
+        add_line(format_item(key, items[key] || default_style))
       end
       
-      add_comment "-------------------------------------"
-      
-      add_comment "Put following into kateschemarc"
+      add_comment "-------------------------------------\n"
+      add_comment "Put following into kateschemarc\n"
+
       add_line "[#{name}]"
 
-      tokens = {
-        "Color Background" => :background,
+      ui = {
+        "Color Background"            => @ui[:background],
 #         "Color Highlighted Bracket" => :,
-        "Color Highlighted Line" => :line_highlight,
+        "Color Highlighted Line"      => @ui[:line_highlight],
 #         "Color Icon Bar" => :,
 #         "Color Line Number" => :,
 #         "Color MarkType1" => :,
@@ -73,7 +82,7 @@ module Coloration
 #         "Color MarkType5" => :,
 #         "Color MarkType6" => :,
 #         "Color MarkType7" => :,
-        "Color Selection" => :selection,
+        "Color Selection"             => @ui[:selection],
 #         "Color Tab Marker" => :,
 #         "Color Template Background" => :,
 #         "Color Template Editable Placeholder" => :,
@@ -82,18 +91,15 @@ module Coloration
 #         "Color Word Wrap Marker" => :
       }
 #       Font=Monospace,9,-1,5,50,0,0,0,0,0
-      tokens.keys.each do |key|
+      ui.keys.each do |key|
 #         add_line(format_token(key, self.send(tokens[key])))
-        add_line("#{key}=#{hex2rgb(self.send(tokens[key]))}")
+        add_line("#{key}=#{hex2rgb(ui[key])}")
       end
       
     end
     
     def hex2rgb(col)
-      r = col[1..2].to_i(16)
-      g = col[3..4].to_i(16)
-      b = col[5..6].to_i(16)
-      "#{r},#{g},#{b}"
+      "#{(col.r*255).to_i},#{(col.g*255).to_i},#{(col.b*255).to_i}"
     end
     
   end
