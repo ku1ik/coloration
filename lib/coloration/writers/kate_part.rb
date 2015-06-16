c3f6d9bee830d9cc7fd5f5fc7d2db2d3c135e6ac
Gavin Laking <gavinlaking@gmail.com>
2015-06-16 19:37:25 +0100

    Whitespace changes.

--- lib/coloration/writers/kate_part.rb
+++ lib/coloration/writers/kate_part.rb
@@ -38,11 +38,10 @@ module Coloration
       def translate
         Coloration.log('Writers::KatePart#translate')
 
-        add_comment comment_message
-        add_comment "-" * 20 + " Put following in katesyntaxhighlightingrc " + "-" * 20
+        add_comment(comment_message)
+        add_comment("-" * 20 + " Put following in katesyntaxhighlightingrc " + "-" * 20)
         store
-
-        store "[Default Item Styles - Schema #{converter.name}]"
+        store("[Default Item Styles - Schema #{converter.name}]")
 
         @default_style = Coloration::Style.new
         @default_style.foreground = ui["foreground"]
@@ -52,7 +51,8 @@ module Coloration
           'Base-N Integer' => items['constant.numeric'],
           'Character'      => items['keyword.operator'],
           'Comment'        => items['comment'],
-          'Data Type'      => items['entity.name.type'] || items['entity.name.class'],
+          'Data Type'      => items['entity.name.type'] ||
+                              items['entity.name.class'],
           'Decimal/Value'  => items['constant.numeric'],
           'Error'          => items['invalid'],
           'Floating Point' => items['constant.numeric'],
@@ -69,57 +69,52 @@ module Coloration
         end
 
         store
-
-        store "[Highlighting Ruby - Schema #{converter.name}]"
-        store 'Ruby:Access Control=0,' + format_style(items['storage.modifier'])
-        store 'Ruby:Command=0,' + format_style(items['string.interpolated'])
-        store 'Ruby:Constant=0,' + format_style(items['constant'])
-        store 'Ruby:Constant Value=0,' + format_style(items['constant.other'])
-        store 'Ruby:Default globals=0,' + format_style(items['variable.other'])
-        store 'Ruby:Definition=0,' + format_style(items['entity.name.function'])
-        store 'Ruby:Delimiter=0,' + format_style(items['keyword.other'])
-        store 'Ruby:Global Constant=0,' + format_style(items['constant.other'])
-        store 'Ruby:Global Variable=0,' + format_style(items['variable.other'])
-        store 'Ruby:Instance Variable=0,' + format_style(items['variable'])
-        store 'Ruby:Class Variable=0,' + format_style(items['variable'])
-        store 'Ruby:Kernel methods=0,' + format_style(items['support.function'])
-        store 'Ruby:Member=0,' + format_style(items['entity.name.function'])
-        store 'Ruby:Message=0,' + format_style(@default_style)
-        store 'Ruby:Operator=0,' + format_style(items['keyword.operator'])
-        store 'Ruby:Pseudo variable=0,' + format_style(items['variable.language'])
-        store 'Ruby:Raw String=0,' + format_style(items['string.quoted.single'])
-  #       store 'Ruby:Region Marker=7,' + format_style(items['])
-        store 'Ruby:Regular Expression=0,' + format_style(items['string.regexp.ruby'])
-        store 'Ruby:Symbol=0,' + format_style(items['constant.other.symbol.ruby'])
-
-        store
-
-        store "[Highlighting JavaScript - Schema #{converter.name}]"
-        store 'JavaScript:Objects=0,' + format_style(items['variable.language'])
+        store("[Highlighting Ruby - Schema #{converter.name}]")
+        store('Ruby:Access Control=0,'     + format_style(items['storage.modifier']))
+        store('Ruby:Command=0,'            + format_style(items['string.interpolated']))
+        store('Ruby:Constant=0,'           + format_style(items['constant']))
+        store('Ruby:Constant Value=0,'     + format_style(items['constant.other']))
+        store('Ruby:Default globals=0,'    + format_style(items['variable.other']))
+        store('Ruby:Definition=0,'         + format_style(items['entity.name.function']))
+        store('Ruby:Delimiter=0,'          + format_style(items['keyword.other']))
+        store('Ruby:Global Constant=0,'    + format_style(items['constant.other']))
+        store('Ruby:Global Variable=0,'    + format_style(items['variable.other']))
+        store('Ruby:Instance Variable=0,'  + format_style(items['variable']))
+        store('Ruby:Class Variable=0,'     + format_style(items['variable']))
+        store('Ruby:Kernel methods=0,'     + format_style(items['support.function']))
+        store('Ruby:Member=0,'             + format_style(items['entity.name.function']))
+        store('Ruby:Message=0,'            + format_style(@default_style))
+        store('Ruby:Operator=0,'           + format_style(items['keyword.operator']))
+        store('Ruby:Pseudo variable=0,'    + format_style(items['variable.language']))
+        store('Ruby:Raw String=0,'         + format_style(items['string.quoted.single']))
+        # store('Ruby:Region Marker=7,'      + format_style(items[']))
+        store('Ruby:Regular Expression=0,' + format_style(items['string.regexp.ruby']))
+        store('Ruby:Symbol=0,'             + format_style(items['constant.other.symbol.ruby']))
 
         store
-
-        store "[Highlighting Ruby/Rails/RHTML - Schema #{converter.name}]"
-        store 'Ruby/Rails/RHTML:Message=0,' + format_style(@default_style)
-        store 'Ruby/Rails/RHTML:Raw String=0,' + format_style(items['string.quoted.single'])
-        store 'Ruby/Rails/RHTML:Symbol=0,' + format_style(items['constant.other.symbol.ruby'])
-        store 'Ruby/Rails/RHTML:Value=0,' + format_style(items['string,string.quoted'])
-        store 'Ruby/Rails/RHTML:Element=0,' + format_style(items['meta.tag'] || items['entity.name.tag'])
-        store 'Ruby/Rails/RHTML:Kernel methods=0,' + format_style(items['support.function'])
-        store 'Ruby/Rails/RHTML:Attribute=0,' + format_style(items['entity.other.attribute-name'])
+        store("[Highlighting JavaScript - Schema #{converter.name}]")
+        store('JavaScript:Objects=0,'      + format_style(items['variable.language']))
 
         store
-
-        store "[Highlighting XML - Schema #{converter.name}]"
-        store 'XML:Value=0,' + format_style(items['string,string.quoted'])
-        store 'XML:Element=0,' + format_style(items['meta.tag'] || items['entity.name.tag'])
-        store 'XML:Attribute=0,' + format_style(items['entity.other.attribute-name'])
+        store("[Highlighting Ruby/Rails/RHTML - Schema #{converter.name}]")
+        store('Ruby/Rails/RHTML:Message=0,'        + format_style(@default_style))
+        store('Ruby/Rails/RHTML:Raw String=0,'     + format_style(items['string.quoted.single']))
+        store('Ruby/Rails/RHTML:Symbol=0,'         + format_style(items['constant.other.symbol.ruby']))
+        store('Ruby/Rails/RHTML:Value=0,'          + format_style(items['string,string.quoted']))
+        store('Ruby/Rails/RHTML:Element=0,'        + format_style(items['meta.tag'] || items['entity.name.tag']))
+        store('Ruby/Rails/RHTML:Kernel methods=0,' + format_style(items['support.function']))
+        store('Ruby/Rails/RHTML:Attribute=0,'      + format_style(items['entity.other.attribute-name']))
 
         store
-        add_comment '-' * 20 + ' Put following in kateschemarc ' + '-' * 20
-        store
+        store("[Highlighting XML - Schema #{converter.name}]")
+        store('XML:Value=0,'      + format_style(items['string,string.quoted']))
+        store('XML:Element=0,'    + format_style(items['meta.tag'] || items['entity.name.tag']))
+        store('XML:Attribute=0,'  + format_style(items['entity.other.attribute-name']))
 
-        store "[#{converter.name}]"
+        store
+        add_comment('-' * 20 + ' Put following in kateschemarc ' + '-' * 20)
+        store
+        store("[#{converter.name}]")
 
         ui_mapping = {
           'Color Background'            => ui['background'],
@@ -143,7 +138,7 @@ module Coloration
           'Color Word Wrap Marker'      => ui['invisibles']
         }
         ui_mapping.keys.each do |key|
-          store "#{key}=#{hex2rgb(ui_mapping[key])}"
+          store("#{key}=#{hex2rgb(ui_mapping[key])}")
         end
 
         retrieve
